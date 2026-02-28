892b0b95006a6f1e59aaf55a031b505994589330
Philipe Xavier <philipexavier@live.com>
2026-02-28 09:41:13 -0300

    next_config.mjs ajustado

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',       // ← obrigatório para o Dockerfile acima
+  output: 'standalone',
   reactStrictMode: true,
-  allowedDevOrigins: [
-    'localhost',
-    '127.0.0.1',
-  ],
 }
 
-module.exports = nextConfig
+export default nextConfig   // ← ES Module usa "export default"
